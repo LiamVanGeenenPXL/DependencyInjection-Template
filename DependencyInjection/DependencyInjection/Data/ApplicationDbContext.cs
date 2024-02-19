6eae38f4278623504c10f04dfdf9cc6c1a75860a
Liam van Geenen <liamvangeenen@gmail.com>
2024-02-19 10:57:54 +0100

    Project startup

---
+++ DependencyInjection/DependencyInjection/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using DependencyInjection.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DependencyInjection.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; } = default!;
+    }
+}
